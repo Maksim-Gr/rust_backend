8e33d81da27fd296f0c98e67071e05a1b234034d
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-07-07 22:58:49 +0300

    chore: employees get method

--- src/employees/routes.rs
+++ src/employees/routes.rs
@@ -1,5 +1,9 @@
+use crate::error_handler::CustomError;
 use actix_web::{delete, get, post, put, web, HttpResponse};
 use serde_json::json;
 
 #[get("/employees")]
-async fn find_all() -> Result<HttpResponse, >
+async fn find_all() -> Result<HttpResponse, CustomError> {
+    todo!()
+}
+
